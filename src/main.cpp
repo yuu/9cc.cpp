0c99632a6bf0fd1e69c390236756d18066b8ae5a
yuu <yuuxzy@gmail.com>
2019-05-21 10:08:19 +0900

    Step 1 impl

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <cstdio>
+#include "cmdline.h"
 
-int main() {
+int main(int argc, char **argv) {
+    cmdline::parser p;
+    p.add<int>("value", 'v', "input value", true, 0);
+    p.parse_check(argc, argv);
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, 42\n");
+    printf("  mov rax, %d\n", p.get<int>("value"));
     printf("  ret\n");
 
     return 0;
--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ try() {
   expected="$1"
   input="$2"
 
-  ./9cc "$input" > tmp.s
+  ./9cc -v "$input" > tmp.s
   gcc -o tmp tmp.s
   ./tmp
   actual="$?"
